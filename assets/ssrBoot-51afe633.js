e933bf733c8e435f1b05c64277d462ee5eb3f524
kuanfuchen <creazy_fu@hotmail.com>
2023-11-19 21:14:27 +0800

    modify header

--- assets/ssrBoot-51afe633.js
+++
@@ -1,1 +0,0 @@
-import{s,o as t,c as e,M as n}from"./index-bcdeeb8a.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
