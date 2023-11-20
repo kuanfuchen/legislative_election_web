a04adfb8b084f0afa499c4eae1ccf40cdeffba47
kuanfuchen <creazy_fu@hotmail.com>
2023-11-21 00:47:02 +0800

    textcolor problem

--- dist/assets/Activity-9ffeb3a1.js
+++
@@ -1,1 +0,0 @@
-import{l as e,F as t,j as r}from"./index-eb2bcde2.js";import c from"./NewActivity-d036c9cf.js";import"./VBtn-3b477684.js";import"./resizeObserver-d2da7b7c.js";const o={class:"d-flex justify-center align-center h-100"},l={__name:"Activity",setup(s){return(a,i)=>(e(),t("div",o,[r(c)]))}};export{l as default};
---
+++ dist/assets/Policy_issues-fa318416.js
@@ -0,0 +1,1 @@
+import e from"./Policity-0c86b6fc.js";import{l as t,F as o,j as r}from"./index-31aa3045.js";import"./plugin-vueexport-helper-c27b6911.js";import"./VRow-e227b8d0.js";const s={class:"d-flex justify-center align-center h-100"},p={__name:"Policy_issues",setup(c){return(i,a)=>(t(),o("div",s,[r(e)]))}};export{p as default};
